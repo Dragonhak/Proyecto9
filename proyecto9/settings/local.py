863c7e767d95976858215fdf45d95aba84af4ad1
portelam91 <pmarcelod@hotmail.com>
2022-08-29 10:55:18 -0300

    h3

--- proyecto9/settings/local.py
+++ proyecto9/settings/local.py
@@ -16,9 +16,9 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'d8iiuus8s8pvor',
-        'USER': 'awveaxpfnczjlt',
-        'PASSWORD': '40526249f2f2ac1ac3c798e2c473552048fa6f4c5939408caf13467694de7534',
+        'NAME':'dfvr27b8bi81ev',
+        'USER': 'lvyjyxjoeobxex',
+        'PASSWORD': 'c432a0de802561fee1325802af57cbfdfbda143471ed0981e91e30b06996677b',
         'HOST': 'ec2-44-205-63-142.compute-1.amazonaws.com',
         'PORT': '5432',
     }
